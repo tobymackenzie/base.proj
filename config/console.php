600a0c6317d5221174266d733733e244b97e160e
Toby Mackenzie <public@tobymackenzie.com>
2022-11-18 16:08:38 -0500

    feat: support local config file with BaseProj options for easier direct use of project locally

--- config/console.php
+++ config/console.php
@@ -1,3 +1,16 @@
 <?php
 //-# only here because I'm not sure how to do this with YAML
-$container->setParameter('paths.project', TJM_PROJECT_DIR);
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+return function(ContainerConfigurator $conf){
+	$params = $conf->parameters();
+	$params->set('baseProjOpts', []);
+	$params->set('paths.project', TJM_PROJECT_DIR);
+	foreach([
+		__DIR__ . '/console.local.php',
+		__DIR__ . '/console.local.yml',
+	] as $file){
+		if(file_exists($file)){
+			$conf->import($file);
+		}
+	}
+};
